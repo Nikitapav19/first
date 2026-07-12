23abeec9893485f2cf269b67fe77134648741797
Nikita <Nikitapav19@gmail.com>
2026-07-12 08:45:06 +0300

    Реализована работа с динамической загрузкой библиотек

---
+++ FileSystemCommands/DirectorySizeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using CommandLib;
+
+namespace FileSystemCommands
+{
+    public class DirectorySizeCommand : ICommand
+    {
+        private readonly string _directoryPath;
+        
+        public long TotalSize { get; private set; }
+
+        public DirectorySizeCommand(string directoryPath)
+        {
+            _directoryPath = directoryPath;
+        }
+
+        public void Execute()
+        {
+            TotalSize = 0;
+            string[] files = Directory.GetFiles(_directoryPath, "*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                TotalSize += info.Length;
+            }
+        }
+    }
+}
---
+++ FileSystemCommands/FindFilesCommand.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.IO;
+using CommandLib;
+
+namespace FileSystemCommands
+{
+    public class FindFilesCommand : ICommand
+    {
+        private string _directoryPath;
+        private string _searchPattern;
+        
+        public List<string> FoundFiles { get; private set; }
+
+        public FindFilesCommand(string directoryPath, string searchPattern)
+        {
+            _directoryPath = directoryPath;
+            _searchPattern = searchPattern;
+            FoundFiles = new List<string>();
+        }
+
+        public void Execute()
+        {
+            FoundFiles.Clear();
+            string[] files = Directory.GetFiles(_directoryPath, _searchPattern, SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                FoundFiles.Add(file);
+            }
+        }
+    }
+}
