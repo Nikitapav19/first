578a7abafce782404d083554496d34fa13a49eb1
Nikita <Nikitapav19@gmail.com>
2026-07-13 11:29:11 +0300

    Система плагинов с графом зависимостей

--- FileSystemCommands/DirectorySizeCommand.cs
+++ FileSystemCommands/DirectorySizeCommand.cs
@@ -1,29 +1,14 @@
-﻿using System.IO;
+﻿using System;
 using CommandLib;
 
 namespace FileSystemCommands
 {
-    [CommandInfo("Вычисляет общий размер всех файлов в каталоге", "Твое Имя")]
+    [PluginLoad("FindFilesCommand")]
     public class DirectorySizeCommand : ICommand
     {
-        private readonly string _directoryPath;
-        
-        public long TotalSize { get; private set; }
-
-        public DirectorySizeCommand(string directoryPath)
-        {
-            _directoryPath = directoryPath;
-        }
-
         public void Execute()
         {
-            TotalSize = 0;
-            string[] files = Directory.GetFiles(_directoryPath, "*", SearchOption.AllDirectories);
-            foreach (string file in files)
-            {
-                FileInfo info = new FileInfo(file);
-                TotalSize += info.Length;
-            }
+            Console.WriteLine("[DirectorySize] выполнена.");
         }
     }
 }
