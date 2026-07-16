b0e1e813f5cbefa3c1cd899d42f26d2a5e6ba087
Nikita <Nikitapav19@gmail.com>
2026-07-16 09:04:29 +0300

    Вычисление определенного интеграла методом трапеции в несколько потоков

---
+++ task14tests/task14tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using task14;
+using Xunit;
+
+namespace task14tests;
+
+public class IntegralTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Func<double, double> X = (double x) => x;
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, X, 1e-4, 2), 1e-4);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        Func<double, double> SIN = (double x) => Math.Sin(x);
+        Assert.Equal(0, DefiniteIntegral.Solve(-1, 1, SIN, 1e-5, 8), 1e-4);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        Func<double, double> X = (double x) => x;
+        Assert.Equal(12.5, DefiniteIntegral.Solve(0, 5, X, 1e-6, 8), 1e-5);
+    }
+}
