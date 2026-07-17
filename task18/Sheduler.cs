0dcbaab2ac40c923ed1e6cc84b4ef094b7c272ff
Nikita <Nikitapav19@gmail.com>
2026-07-17 13:59:17 +0300

    Реализация планировщика команд

---
+++ task18/Sheduler.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public interface IScheduler
+{
+    bool HasCommand();
+    ICommand Select();
+    void Add(ICommand cmd);
+}
+
+public class RoundRobinScheduler : IScheduler
+{
+    private readonly ConcurrentQueue<ICommand> _commands = new ConcurrentQueue<ICommand>();
+    public AutoResetEvent SyncEvent { get; set; }
+
+    public bool HasCommand()
+    {
+        return !_commands.IsEmpty;
+    }
+
+    public ICommand Select()
+    {
+        _commands.TryDequeue(out var cmd);
+        return cmd;
+    }
+
+    public void Add(ICommand cmd)
+    {
+        _commands.Enqueue(cmd);
+        SyncEvent?.Set();
+    }
+}
+
+public class ThreadQueue
+{
+    private readonly ConcurrentQueue<ICommand> _queue = new ConcurrentQueue<ICommand>();
+    private readonly AutoResetEvent _syncEvent;
+    public bool IsAddingCompleted { get; private set; }
+
+    public ThreadQueue(AutoResetEvent syncEvent)
+    {
+        _syncEvent = syncEvent;
+    }
+
+    public void Add(ICommand cmd)
+    {
+        _queue.Enqueue(cmd);
+        _syncEvent.Set();
+    }
+
+    public bool TryTake(out ICommand cmd)
+    {
+        return _queue.TryDequeue(out cmd);
+    }
+
+    public bool IsEmpty => _queue.IsEmpty;
+
+    public void CompleteAdding()
+    {
+        IsAddingCompleted = true;
+        _syncEvent.Set();
+    }
+}
+
+public static class ExceptionHandler
+{
+    public static void Handle(Exception ex, ICommand command)
+    {
+    }
+}
+
+public class ServerThread
+{
+    public ThreadQueue Queue { get; }
+    public IScheduler Scheduler { get; }
+    public Thread Thread { get; }
+    public Action Behavior { get; set; }
+    public bool IsStopped { get; private set; }
+
+    public readonly AutoResetEvent SyncEvent = new AutoResetEvent(false);
+    private bool _takeFromSchedulerNext = false;
+
+    public ServerThread(IScheduler scheduler)
+    {
+        Queue = new ThreadQueue(SyncEvent);
+        Scheduler = scheduler;
+        
+        if (Scheduler is RoundRobinScheduler rrScheduler)
+        {
+            rrScheduler.SyncEvent = SyncEvent;
+        }
+
+        Behavior = DefaultBehavior;
+        Thread = new Thread(Run);
+    }
+
+    private void Run()
+    {
+        while (!IsStopped)
+        {
+            Behavior();
+        }
+    }
+
+    public bool TryGetNextCommand(out ICommand cmd)
+    {
+        cmd = null;
+        bool hasSchedulerCmd = Scheduler.HasCommand();
+        bool hasQueueCmd = !Queue.IsEmpty;
+
+        if (hasSchedulerCmd && hasQueueCmd)
+        {
+            if (_takeFromSchedulerNext)
+            {
+                cmd = Scheduler.Select();
+                _takeFromSchedulerNext = false;
+            }
+            else
+            {
+                Queue.TryTake(out cmd);
+                _takeFromSchedulerNext = true;
+            }
+        }
+        else if (hasSchedulerCmd)
+        {
+            cmd = Scheduler.Select();
+        }
+        else if (hasQueueCmd)
+        {
+            Queue.TryTake(out cmd);
+        }
+
+        return cmd != null;
+    }
+
+    private void DefaultBehavior()
+    {
+        while (!Scheduler.HasCommand() && Queue.IsEmpty && !IsStopped && !Queue.IsAddingCompleted)
+        {
+            SyncEvent.WaitOne();
+        }
+
+        if (IsStopped) return;
+
+        if (TryGetNextCommand(out ICommand cmd))
+        {
+            try
+            {
+                cmd.Execute();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.Handle(ex, cmd);
+            }
+        }
+        else if (Queue.IsAddingCompleted)
+        {
+            Stop();
+        }
+    }
+
+    public void Start()
+    {
+        Thread.Start();
+    }
+
+    public void Stop()
+    {
+        IsStopped = true;
+        SyncEvent.Set();
+    }
+}
+
+public class HardStopCommand : ICommand
+{
+    private readonly ServerThread _serverThread;
+
+    public HardStopCommand(ServerThread serverThread)
+    {
+        _serverThread = serverThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _serverThread.Thread)
+        {
+            throw new InvalidOperationException();
+        }
+
+        _serverThread.Stop();
+    }
+}
+
+public class SoftStopCommand : ICommand
+{
+    private readonly ServerThread _serverThread;
+
+    public SoftStopCommand(ServerThread serverThread)
+    {
+        _serverThread = serverThread;
+    }
+
+    public void Execute()
+    {
+        if (Thread.CurrentThread != _serverThread.Thread)
+        {
+            throw new InvalidOperationException();
+        }
+
+        _serverThread.Queue.CompleteAdding();
+        _serverThread.Behavior = SoftStopCommandBehavior;
+    }
+
+    private void SoftStopCommandBehavior()
+    {
+        if (_serverThread.TryGetNextCommand(out ICommand cmd))
+        {
+            try
+            {
+                cmd.Execute();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.Handle(ex, cmd);
+            }
+        }
+        else
+        {
+            _serverThread.Stop();
+        }
+    }
+}
---
+++ task18tests/ShedulerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+public class LongRunningCommand : ICommand
+{
+    private IScheduler _scheduler;
+    private int _totalSteps;
+    public int Steps { get; private set; }
+
+    public LongRunningCommand(IScheduler scheduler, int totalSteps)
+    {
+        _scheduler = scheduler;
+        _totalSteps = totalSteps;
+        Steps = 0;
+    }
+
+    public void Execute()
+    {
+        Steps++;
+        if (Steps < _totalSteps)
+        {
+            _scheduler.Add(this);
+        }
+    }
+}
+
+public class ServerThreadSchedulerTests
+{
+    [Fact]
+    public void Scheduler_ExecutesLongCommandToTheEnd()
+    {
+        var scheduler = new RoundRobinScheduler();
+        var server = new ServerThread(scheduler);
+        var longCmd = new LongRunningCommand(scheduler, 5);
+        var softStop = new SoftStopCommand(server);
+
+        server.Queue.Add(longCmd);
+        server.Queue.Add(softStop);
+
+        server.Start();
+        server.Thread.Join(2000);
+
+        Assert.True(server.IsStopped);
+        Assert.Equal(5, longCmd.Steps);
+    }
+
+    [Fact]
+    public void Scheduler_SwitchesBetweenCommands()
+    {
+        var scheduler = new RoundRobinScheduler();
+        var server = new ServerThread(scheduler);
+        var cmd1 = new LongRunningCommand(scheduler, 2);
+        var cmd2 = new LongRunningCommand(scheduler, 2);
+        var softStop = new SoftStopCommand(server);
+
+        server.Queue.Add(cmd1);
+        server.Queue.Add(cmd2);
+        server.Queue.Add(softStop);
+
+        server.Start();
+        server.Thread.Join(2000);
+
+        Assert.Equal(2, cmd1.Steps);
+        Assert.Equal(2, cmd2.Steps);
+    }
+}
